40dcfadf7e77882e44f35898214364e49ccce74d
Nico Steppat <steppat@gmail.com>
2020-09-27 13:01:08 -0300

    video5.2 Primeiro exemplo com CriteriaQuery

--- src/main/java/br/com/alura/jpa/testes/TestaSomaDasMovimentacoes.java
+++ src/main/java/br/com/alura/jpa/testes/TestaSomaDasMovimentacoes.java
@@ -1,10 +1,17 @@
 package br.com.alura.jpa.testes;
 
+import java.math.BigDecimal;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.Root;
 
-import br.com.alura.jpa.modelo.dao.MovimentacaoDao;
+import br.com.alura.jpa.modelo.Movimentacao;
 
 public class TestaSomaDasMovimentacoes {
 	public static void main(String[] args) {
@@ -12,8 +19,21 @@ public class TestaSomaDasMovimentacoes {
     	EntityManagerFactory emf = Persistence.createEntityManagerFactory("alura");
 		EntityManager em = emf.createEntityManager();
 
-		MovimentacaoDao dao = new MovimentacaoDao(em);
-		System.out.println("A soma das movimentacoes é: " + dao.getSomaDasMovimentacoes());
+		CriteriaBuilder builder = em.getCriteriaBuilder();
+		CriteriaQuery<BigDecimal> query = builder.createQuery(BigDecimal.class);
+		
+		//from Movimentacao m
+		Root<Movimentacao> root = query.from(Movimentacao.class);
+		
+		//sum(m.valor)
+		Expression<BigDecimal> sum = builder.sum(root.<BigDecimal>get("valor"));
+		
+		//select sum(..)
+		query.select(sum);
+		
+		TypedQuery<BigDecimal> typedQuery = em.createQuery(query);
+		
+		System.out.println("A soma das movimentacoes é: " + typedQuery.getSingleResult());
 
 	}
 }

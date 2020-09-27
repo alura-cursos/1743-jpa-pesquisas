27f612a50ac389f8d5e1914d09c6400df49ca4dd
Nico Steppat <steppat@gmail.com>
2020-09-27 11:26:19 -0300

    video3.4

--- src/main/java/br/com/alura/jpa/testes/TestaMediaDiariaDasMovimentacoes.java
+++ src/main/java/br/com/alura/jpa/testes/TestaMediaDiariaDasMovimentacoes.java
@@ -5,20 +5,23 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+
+import br.com.alura.jpa.modelo.MediaComData;
 
 public class TestaMediaDiariaDasMovimentacoes {
 	public static void main(String[] args) {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("alura");
 		EntityManager em = emf.createEntityManager();
 		
-		String jpql = "select avg(m.valor), day(m.data), month(m.data) from Movimentacao m group by day(m.data), month(m.data), year(m.data)";
+		String jpql = "select new br.com.alura.jpa.modelo.MediaComData(avg(m.valor), day(m.data), month(m.data)) from Movimentacao m "
+						+ "group by day(m.data), month(m.data), year(m.data)";
 
-		Query query = em.createQuery(jpql);
-		List<Object[]> mediaDasMovimentacoes = query.getResultList();
+		TypedQuery<MediaComData> query = em.createQuery(jpql, MediaComData.class);
+		List<MediaComData> mediaDasMovimentacoes = query.getResultList();
 		
-		for (Object[] resultado: mediaDasMovimentacoes) {
-			System.out.println("A média das movimentacoes do dia " + resultado[1] + "/" + resultado[2] + " é: " + resultado[0]);
+		for (MediaComData resultado : mediaDasMovimentacoes) {
+			System.out.println("A média das movimentacoes do dia " + resultado.getDia() + "/" + resultado.getMes() + " é: " + resultado.getValor());
 		}
 	}
 

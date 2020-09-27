de5df085bf8c193d90f9ff99861cb713d30082b1
Nico Steppat <steppat@gmail.com>
2020-09-27 12:54:10 -0300

    video5.1 JPQL dinamico

--- src/main/java/br/com/alura/jpa/modelo/dao/MovimentacaoDao.java
+++ src/main/java/br/com/alura/jpa/modelo/dao/MovimentacaoDao.java
@@ -7,6 +7,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 
 import br.com.alura.jpa.modelo.MediaComData;
+import br.com.alura.jpa.modelo.Movimentacao;
 
 public class MovimentacaoDao {
 
@@ -14,6 +15,25 @@ public class MovimentacaoDao {
 	
 	public MovimentacaoDao(EntityManager em) {
 		this.em = em;
+	}
+	
+	public List<Movimentacao> getMovimentacoesFiltradasPorData(Integer dia, Integer mes, Integer ano) {
+		
+		String sql = "select m from Movimentacao m ";
+		
+		if(dia != null) {
+			sql += " day(m.data) = :pDia";
+		}
+		
+		if(mes != null) {
+			sql += " and month(m.data) = :pMes";
+		}
+		
+		if(ano != null) {
+			sql += " and year(m.data) = :pAno";
+		}
+		
+		return null;
 	}
 
 	public List<MediaComData> getMediaDiariaDasMovimentacoes() {

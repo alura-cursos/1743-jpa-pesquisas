11127e781cfe21f515a67724d255a24cbdec5423
Nico Steppat <steppat@gmail.com>
2020-09-27 09:33:35 -0300

    video1.3

--- src/main/java/br/com/alura/jpa/modelo/Conta.java
+++ src/main/java/br/com/alura/jpa/modelo/Conta.java
@@ -1,9 +1,12 @@
 package br.com.alura.jpa.modelo;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Conta {
@@ -14,14 +17,14 @@ public class Conta {
 	private Integer agencia;
 	private Integer numero;
 	private Double saldo;
+	
+	@OneToMany
+	private List<Movimentacao> movimentacoes;
 
 	public void setSaldo(Double saldo) {
 		this.saldo = saldo;
 	}
 	
-	
-
-
 	public Long getId() {
 		return id;
 	}
@@ -52,5 +55,10 @@ public class Conta {
 
 	public void setNumero(Integer numero) {
 		this.numero = numero;
+	}
+
+
+	public List<Movimentacao> getMovimentacoes() {
+		return this.movimentacoes;
 	}
 }

7a2d1a50c8b373499a3bcdb08ce9c1d9cd8e0192
Nico Steppat <steppat@gmail.com>
2020-09-27 10:10:37 -0300

    video2.2

--- src/main/java/br/com/alura/jpa/modelo/Conta.java
+++ src/main/java/br/com/alura/jpa/modelo/Conta.java
@@ -3,6 +3,7 @@ package br.com.alura.jpa.modelo;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -18,7 +19,7 @@ public class Conta {
 	private Integer numero;
 	private Double saldo;
 	
-	@OneToMany(mappedBy = "conta")
+	@OneToMany(mappedBy = "conta", fetch=FetchType.EAGER)
 	private List<Movimentacao> movimentacoes;
 
 	public void setSaldo(Double saldo) {
